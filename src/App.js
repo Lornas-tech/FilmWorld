415f74a7e6f1d5b01bbb558b3305c0e0679ee49c
Lorna Stamat <77707894+Lornas-tech@users.noreply.github.com>
2022-09-06 16:34:21 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=33cb9cd4";
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=33cb9cd4";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
